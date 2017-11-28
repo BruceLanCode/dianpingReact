9b84b26ca1ca08d9b88d3506fa2dcf17dcbe81d4
lantuhui <shengxiaobai15@163.com>
2017-11-28 09:33:40 +0800

    finish mock
    finish mock

--- mock/server.js
+++ mock/server.js
@@ -10,13 +10,41 @@ const router = new Router();
 
 const homeAdData = require('./home/ad');
 router.get('/api/homead',(cxt) => {
-  cxt.response.body = homeAdData;
+    cxt.response.body = homeAdData;
 });
 
 const homeListData = require('./home/list');
 router.get('/api/homeList/:city/:page',(cxt) => {
-  cxt.response.body = homeListData;
-  console.log();
+    cxt.response.body = homeListData;
+    console.log();
+});
+
+const searchListData = require('./search/list');
+router.get('/api/search/:page/:city/:category/:keyword',(cxt) => {
+    cxt.response.body = searchListData;
+});
+
+const detailInfo = require('./detail/info.js');
+router.get('/api/detail/info/:id',(cxt) => {
+    cxt.response.body = detailInfo;
+});
+
+const detailComment = require('./detail/comment.js');
+router.get('/api/detail/comment/:page/:id',(cxt) => {
+    cxt.response.body = detailComment;
+});
+
+const orderList = require('./detail/orderList.js');
+router.get('/api/detail/orderList/:username',(cxt) => {
+    cxt.response.body = orderList;
+});
+
+router.post('/api/submitCommet',(cxt) => {
+    console.log('提交评论');
+    cxt.response.body = {
+        errno: 0,
+        msg: 'ok'
+    };
 });
 
 app.use(router.routes());
