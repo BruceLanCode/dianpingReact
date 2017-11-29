d3c676585326cbf16e4a803c6bac7c4f9133b958
lantuhui <shengxiaobai15@163.com>
2017-11-29 22:25:30 +0800

    create reducers

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+import RouteMap from './router/routeMap';
+
 import './static/css/common.less';
 import './static/css/font.less';
+
+const store = configureStore();
 
 render(
     <div>lantu!!!! <i className="icon-arrow-left"></i></div>,
---
+++ app/router/routeMap.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router,Route } from 'react-router-dom';
+
+class RouteMap extends Component {
+    render(){
+        return (
+            <div>huangxi</div>
+        )
+    }
+}
+
+export default RouteMap;
